e6ab16b17cf6bb2321d9e46041c22de825e433ad
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-01-20 17:08:07 -0300

    Relatório de alunos com status pré-matriculado que estão com contrato assinado.

---
+++ relatorio.py
@@ -0,0 +1,45 @@
+##Relatório que traz alunos pré-matriculados com contrato assinado
+import streamlit as st
+import pyodbc
+from dotenv import load_dotenv
+import pandas as pd
+from datetime import date
+
+today = date.today()
+hoje = today.strftime("%d-%m-%Y")
+
+load_dotenv()
+
+dbUser = st.secrets['DB_USER']
+dbPass = st.secrets['DB_PASS']
+database = st.secrets['DB_NAME']
+server = st.secrets['DB_IP']
+query = st.secrets['QUERY']
+
+print(server, database, dbUser, dbPass)
+
+st.title('Alunos pré-matriculados com contrato assinado, período 231.')
+#Conexão com o banco de dados
+connection = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + server
+        + ";DATABASE="
+        + database
+        + ";UID="
+        + dbUser
+        + ";PWD="
+        + dbPass
+)
+cursor = connection.cursor()
+
+df = pd.read_sql(query,connection)
+
+df.to_excel(f'{hoje}.xlsx', index=False)
+
+with open(f'{hoje}.xlsx', 'rb') as excel:
+    st.download_button("Gerar relatório", excel, f'{hoje}.xlsx')
+
+connection.commit()
+connection.close()
+
+
